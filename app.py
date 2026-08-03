50a4aa1f25fbc21ba465f0942cf5d1c717de186e
fadilrawther <fadilrawther@gmail.com>
2026-08-02 22:34:04 -0700

    Integrate Google Sheets for order submissions
    Updated the application to connect to Google Sheets for order submission instead of CSV. Enhanced the UI for uniform order form with additional fields.

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 import streamlit as st
-import csv
+import gspread
+from google.oauth2.service_account
+import Credentials
+#connect to google sheets
+scope=
+["https://www.ggleapis.com/auth/spreadsheets"]
 
-st.title("Uniform order Form")
+creds=
+Credentials.from_service_account_file("credentials.json",scope=scope)
+client=gspread.authorize(creds)
 
-name=st.text_input("Name")
-phone=st.text_input("Phone")
-quantity=st.number_input("Quantity",min_value=1)
+sheet=client.open("Client Data").sheet1
+#UI
+st.title("MIHANIYY School Uniform Form")
+school=st.text_input("School Name")
+class_name=st.text_input("class")
+uniform=st.selecttbox("Uniform Type",["Shirt","T-Shirt"])
+bottom=st.selectbox("Bottom Type",["Shorts","Pants"])
+colour=st.selectbox("colour",["Red","Blue","Green","Yellow"])
+size=st.selectbox("Size",["XS","S","M","L","XL","XXL","3XL","5XL"])
+qty=st.number_input("Quantity",min_value=1)
+#submit
+if st.button("Submit Order"):
+    sheet.append_rpw([
+        school,
+        class_name,
+        uniform,
+        bottom,
+        colour,
+        size,
+        qty
+    ])
+    st.success("Order submitted successfully!")
 
-if st.button("Submit"):
-    with open("Orders.csv","a", newline="")as f:
-        writer=csv.writer(f)
-        writer.writerow([name,phone,quantity])
-        st.success("Ordersubmitted!")
