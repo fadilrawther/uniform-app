851f1bfd0dfba706c36e5cd9623d8751938bd6b7
fadilrawther <fadilrawther@gmail.com>
2026-08-02 22:55:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ import streamlit as st
 import gspread
 from google.oauth2 import 
 service_account
+from google.oauth2.service_account
 import Credentials
 #connect to google sheets
 scope=
-["https://www.ggleapis.com/auth/spreadsheets"]
+["https://www.googleapis.com/auth/spreadsheets"]
 
 creds=
-Credentials.from_service_account_file("credentials.json",scope=scope)
+Credentials.from_service_account_file(
+    "credentials.json",
+    scope=scope
+)    
 client=gspread.authorize(creds)
 
 sheet=client.open("Client Data").sheet1
 #UI
 st.title("MIHANIYY School Uniform Form")
 school=st.text_input("School Name")
-class_name=st.text_input("class")
+class_name=st.text_input("Class")
 uniform=st.selecttbox("Uniform Type",["Shirt","T-Shirt"])
 bottom=st.selectbox("Bottom Type",["Shorts","Pants"])
 colour=st.selectbox("colour",["Red","Blue","Green","Yellow"])
@@ -23,7 +27,7 @@ size=st.selectbox("Size",["XS","S","M","L","XL","XXL","3XL","5XL"])
 qty=st.number_input("Quantity",min_value=1)
 #submit
 if st.button("Submit Order"):
-    sheet.append_rpw([
+    sheet.append_row([
         school,
         class_name,
         uniform,
